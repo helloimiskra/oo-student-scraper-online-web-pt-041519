0b7afc6cb9ef8a69d71cd5c973f0f0ca5ed0dd75
Iskra B <ibatistapoblete@gmail.com>
2019-06-17 02:09:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,34 +16,29 @@ class Scraper
       }
       s
     end
+
+    # binding.pry
+    # s_names = url.css('div.student-card h4.student-name').map{|el| el.text}
+    # s_locations = url.css('div.student-card p.student-location').map{|el| el.text}
+    # profile_url = url.css('div.student-icon-container a').map { |link| link['href'] }[1]
+
   end
 
   def self.scrape_profile_page(profile_url)
-    url = Nokogiri::HTML(open('./fixtures/student-site/students/david-kim.html'))
+    url = Nokogiri::HTML(open(profile_url))
     social = url.css('div.social-icon-container a').map { |link| nil ? nil : link['href'] || nil }
+    #  binding.pry
     profile_details = {
-      :linkedin => social.map do |x|
-        if x.include?("linkedin")
-          x
-        end
-      end,
-      :github => social.map do |x|
-        if x.include?("github")
-          x
-        end
-      end,
-      :blog => social.map do |x|
-        if x.include?(".com")
-          x
-        end
-      end,
-
-      :twitter => social.map do |x|
-        if x.include?("twitter")
-          x
-        end
-      end,
-
+      :linkedin => social[1],
+      :github => social[2],
+      # social.each {|social| social.include?('github') ? social : nil},
+      # social[2].include?('github') ? social[2] : nil,
+      :blog => social[3],
+      # social.each {|social| social.include?('.com') ? social : nil},
+      # # social[3].include?('.com') ? social[3] : nil,
+      :twitter => social[0],
+      # social.each {|social| social.include?('twitter') ? social : nil},
+      # social[0].include?('twitter') ? social[0] : nil,
       :profile_quote => url.css('div.profile-quote').text.strip,
       :bio => url.css('div.description-holder p').text.strip
     }
